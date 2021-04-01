8d10313a0da5e7db3d01ebea8cb8520d8eded57d
lu <919041098@qq.com>
2021-04-01 10:40:33 +0800

    创建createId来自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {createId} from './lib/createId';
 type Tags = {
   id:number;
   name:string
@@ -6,10 +7,10 @@ type Tags = {
 const useTags = ()=> {
   const [tags, setTags] = useState<Tags[]>(
     [
-      {id: 1, name: '衣'},
-      {id: 2, name: '食'},
-      {id: 3, name: '住'},
-      {id: 4, name: '行'}
+      {id: createId(), name: '衣'},
+      {id: createId(), name: '食'},
+      {id: createId(), name: '住'},
+      {id: createId(), name: '行'}
     ]
   )
   return {tags,setTags}
