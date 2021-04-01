6e5aff905b57e8478e398d261bd5360c9bb9b2a0
lu <919041098@qq.com>
2021-04-01 09:28:41 +0800

    Label list 样式

--- src/views/Label.tsx
+++ src/views/Label.tsx
@@ -1,17 +1,58 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d5;
+    line-height: 28px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #9a9a9a;
+  border-radius: 4px;
+  color:white;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const Space = styled.div`
+  height: 16px;
+  
+`
 function Label() {
-  const {tags,setTags} = useTags()
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map(tags =>
-          <li key={tags}>{tags}</li>
+          <li key={tags}><span className="oneLine">{tags}</span> <Icon name="right"/></li>
         )}
-      </ol>
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
+
     </Layout>
   );
 }
-export default Label
+
+export default Label;
